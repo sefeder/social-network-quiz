ead80cfe30f6823d3c1fc355b2a7bfb551b51888
Seth Feder <sethfeder2017@u.northwestern.edu>
2018-09-29 23:08:04 -0500

    main logic, basic front-end complete

---
+++ assets/javascript.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+});
+
+limitChecks = limits => {
+    limits.forEach(limit => {
+        $(`input.limit-${limit}`).on('change', function (evt) {
+            if ($(this).parent().parent().find('input:checked').length > limit) {
+                this.checked = false;
+            }
+        });
+
+    })
+    
+}
+limitChecks([1,2,3]);
+
+$('#submit').on('click', function(event){
+    let finalSums = [0,0,0,0,0,0]
+   let checkedInputs = $('#questions').find('input:checked')
+   checkedInputs.each((idx, input) =>{
+        let values = JSON.parse(input.value)
+        console.log(idx, values)
+       for (let i = 0; i < finalSums.length; i++) {
+           finalSums[i] = finalSums[i] + values[i];
+       }
+   })
+   console.log("final sums: ", finalSums)
+
+let platformArray = [
+    {
+        platform: 'Facebook',
+        value: finalSums[0]
+    },
+    {
+        platform: 'Instagram',
+        value: finalSums[1]
+    },
+    {
+        platform: 'Twitter',
+        value: finalSums[2]
+    },
+    {
+        platform: 'LinkedIn',
+        value: finalSums[3]    
+    },
+    {
+        platform: 'Pinterest',
+        value: finalSums[4]
+    },
+    {
+        platform: 'YouTube',
+        value: finalSums[5]
+    }
+]
+
+console.log('platformArray: ', platformArray)
+
+let sortedArray = platformArray.slice(0)
+sortedArray.sort(function(a,b){
+   return b.value-a.value
+})
+console.log('sortedArray: ', sortedArray)
+
+})
